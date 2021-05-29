38f5ef6ff65a3bf65158e66526e85338cec006af
Paul Everitt <pauleveritt@me.com>
2021-05-29 12:06:07 -0400

    Only support Python 3.9

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ except ImportError:
 
 
 package = "hopscotch"
-python_versions = ["3.9", "3.8", "3.7", "3.6"]
+python_versions = ["3.9"]
 nox.options.sessions = (
     "pre-commit",
     "safety",
