72d7c52d50c6b02d99fadb79113a5a056814e95a
Paul Everitt <pauleveritt@me.com>
2021-10-03 12:25:24 -0400

    Upgrade dependencies.

--- tests/test_field_infos.py
+++ tests/test_field_infos.py
@@ -1,5 +1,7 @@
 """Test the field discovery functions for various targets."""
 import typing
+
+import pytest
 from hopscotch import Registry
 from hopscotch.field_infos import FieldInfo
 from hopscotch.field_infos import get_dataclass_field_infos
@@ -18,8 +20,6 @@ from hopscotch.fixtures.dataklasses import GreetingOperator
 from hopscotch.fixtures.dataklasses import GreetingTuple
 from hopscotch.operators import Get
 from hopscotch.operators import Operator
-
-import pytest
 
 
 @pytest.mark.parametrize(
